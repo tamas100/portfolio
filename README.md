254e8962f9e2f2f6c77af35edf9469a658628b9a
Tamás Kis <t.kis502@gmail.com>
2023-06-09 11:15:06 +0200

    Modify navbar at 1200px. Add new style to profile-img. No flexbox-direction:column in section-home. Add more gap between project-cards. Resize queen.png. Transfer styles of css validator to styles.css. Add the url of the homepage to readme.md. Delete unnecessary favicons. Add comments to main.js.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Portfólió
-Statikus portfólió oldal három törésponttal.  
-A [Karrierváltó Mentorprogram](https://programozaskarrier.hu/karriervalto/) projektfeladata.
+
+Statikus portfólió oldal három törésponttal.
+A [Karrierváltó Mentorprogram](https://programozaskarrier.hu/karriervalto/) projektfeladata.  
+A honlap a github-pages felületen érhető el. [Kis Tamás portfólió oldala](https://tamas100.github.io/portfolio/).
--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
+// Hamburger menu
+
+// variables
 let button = document.getElementById("button-js");
 let OpenC = document.querySelector("#menu-list");
 let icon = document.getElementById("button-icon");
 
+// Eventlistener
 button.addEventListener("click", Hmenu);
 OpenC.addEventListener("click", Hmenu)
 
+// 
 function Hmenu() {
-    OpenC.classList.toggle("active");
+    // add or remove the class "active" to the menu-list
+    OpenC.classList.toggle("active");  
+    // if menu-list has the class "active" then change button-icon to bi-x
     if(OpenC.classList === "active") {
         icon.classList.toggle("bi-x");
+    // else return the orignal button-icon   
     } else {
         icon.classList.toggle("bi-x");
     }
